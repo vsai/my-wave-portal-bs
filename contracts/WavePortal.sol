68ac762f46d7f2386866c12318d4d8148bd95a4d
vsai <vishalsai@gmail.com>
2022-06-28 16:26:33 +0800

    waves: add more waving functionality and functionality to WaveContract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,22 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    mapping(address => uint256) wavers;
+
     constructor() {
         console.log("YSM Hello World Smart Contract");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        wavers[msg.sender] += 1;
+        console.log("%s has waved!", msg.sender);
+        console.log("%s: wave count: %d", msg.sender, wavers[msg.sender]);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We had %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
